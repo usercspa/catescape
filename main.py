412b4881cdff71a0f529d45b37f5b11166026bb2
usercspa <spacchbk@gmail.com>
2021-01-23 01:45:41 +0000

    debug

--- main.py
+++ main.py
@@ -26,44 +26,65 @@ cat_width = 56
 catimg = pygame.image.load("cat.gif")
 grass = pygame.image.load("grass.png")
 human = pygame.image.load("human.png")
+opening = pygame.image.load("opening.png")
 
 #cat position 
 def cat(x,y):
   screen.blit(catimg, (x, y))
 
-#text
-def text_objects(text, font):
-    textSurface = font.render(text, True, black)
-    return textSurface, textSurface.get_rect()  
 
-#message
-def message_display(text):
-    largeText = pygame.font.Font('None',115)
-    TextRect = text_objects(text, largeText)
-    TextRect.center = ((WIDTH/2),(HEIGHT/2))
-    screen.blit(TextRect)
 
-    pygame.display.update()
-
-    time.sleep(2)
-
-    game_loop()
+#obstacles
+def obstacles(obx, oby, obw, obh, color):
+    pygame.draw.rect(screen, color, [obx, oby, obw, obh])    
 
 #caught
 def caught():
-    message_display('You got caught')
-    
+    display_message('You got caught')
+
+
+#score
+def obstacle_dodged(count):
+    font = pygame.font.SysFont(None, 25)
+    text = font.render("Dodged: "+str(count), True, black)
+    screen.blit(text,(350,50))
+
+#start screen
+def game_opening():
+    screen.blit(opening,(0,0))
+    pygame.display.update()
+    time.sleep(1)    
+
+#end screen
+def display_message(content):
+    pygame.time.delay(500)
+    screen.fill(black)
+    font = pygame.font.SysFont(None, 100)
+    text = font.render(("You got caught"), True, white)
+    screen.blit(text, ((WIDTH - text.get_width()) // 2, (WIDTH - text.get_height()) // 2))
+    pygame.display.update()
+    pygame.time.delay(3000)
+
+#game loop    
 def game_loop():
+
+    game_opening()
+
+
     x = (WIDTH * 0.45)
     y = (HEIGHT * 0.8)
 
     x_change = 0
 
-    human_startx = random.randrange(0, WIDTH)
-    human_starty = -600
-    human_speed = 7
-    human_width = 100
-    human_height = 100
+    ob_startx = random.randrange(0, WIDTH)
+    ob_starty = -600
+    ob_speed = 7
+    ob_width = 100
+    ob_height = 100
+
+    obCount = 1
+
+    dodged = 0
 
     gameExit = False
 
@@ -87,24 +108,28 @@ def game_loop():
         x += x_change
         screen.fill(grey)
 
-        human(human_startx, human_starty, human_width, human_height)
-        human_starty += human_speed
+        obstacles(ob_startx, ob_starty, ob_width, ob_height, white)
+        ob_starty += ob_speed
         cat(x,y)
+        obstacle_dodged(dodged)
 
         if x > WIDTH - cat_width or x < 0:
             caught()
 
-        if human_starty > HEIGHT:
-            human_starty = 0 - human_height
-            human_startx = random.randrange(0,WIDTH)
+        if ob_starty > HEIGHT:
+            ob_starty = 0 - ob_height
+            ob_startx = random.randrange(0,WIDTH)
+            dodged += 1
+            ob_speed += 1
+            ob_width += (dodged * 1.2)
 
 
-        if y < human_starty+human_height:
-            print('y crossover')
-
-            if x > human_startx and x < human_startx + human_width or x+cat_width > human_startx and x + cat_width < human_startx+human_width:
-                print('x crossover')
-                caught()
+        if y < ob_starty+ob_height:
+            
+            if x > ob_startx and x < ob_startx + ob_width or x+cat_width > ob_startx and x + cat_width < ob_startx+ob_width:
+                
+          
+              caught()
 
         
         pygame.display.update()
